b6e006bc30c8f83a4fd249798c933663d29a2593
jserme <dev.hubo@gmail.com>
2017-04-25 19:25:59 +0800

    add tests

---
+++ test/fixtures/simple.md
@@ -0,0 +1,29 @@
+<a name="0.1.0"></a>
+# some thing 
+
+
+### Bug Fixes
+
+* fixa ([17c2c43](https://abc/commit/abc))
+* fixb ([17c2c43](https://abc/commit/abc))
+
+
+### Features
+
+* feata ([17c2c43](https://abc/commit/abc))
+* featb ([17c2c43](https://abc/commit/abc))
+
+
+<a name="0.0.1"></a>
+# some thing 
+
+### Bug Fixes
+
+* fixc ([17c2c43](https://abc/commit/abc))
+* fixd ([17c2c43](https://abc/commit/abc))
+
+
+### Features
+
+* featc ([17c2c43](https://abc/commit/abc))
+* featd ([17c2c43](https://abc/commit/abc))
---
+++ test/index.js
@@ -0,0 +1,45 @@
+/* eslint-env mocha */
+const fs = require('fs')
+const path = require('path')
+const parser = require('../')
+const assert = require('assert')
+function getFix (name) {
+  return path.join(__dirname, `./fixtures/${name}`)
+}
+
+function readFix (name) {
+  return fs.readFileSync(getFix(name), 'utf-8')
+}
+
+describe('cclog-parser', function () {
+  const empty = readFix('empty.md')
+  const simple = readFix('simple.md')
+  const karma = readFix('karma.md')
+
+  it('simple parse', function () {
+    const rst = parser(simple)
+
+    assert(rst.versions.length === 2)
+    assert(rst.versions.includes('0.0.1'))
+    assert(rst.versions.includes('0.1.0'))
+
+    assert(Object.keys(rst.changes).includes('0.0.1'))
+    assert(rst.changes['0.0.1'].fixes.length === 2)
+    assert(rst.changes['0.0.1'].features.length === 2)
+
+    assert(rst.changes['0.0.1'].fixes[0] === 'fixc ([17c2c43](https://abc/commit/abc))')
+  })
+
+  it('empty parse', function () {
+    const rst = parser(empty)
+    assert(rst.versions.length === 0)
+    assert(Object.keys(rst.changes).length === 0)
+  })
+
+  it('large parse', function () {
+    const rst = parser(karma)
+    assert(rst.versions.length > 10)
+    assert(Object.keys(rst.changes).length > 10)
+    assert(Object.keys(rst.changes).length === rst.versions.length)
+  })
+})
